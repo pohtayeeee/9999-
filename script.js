16c66d8ec00772ce32689cb61a48d2bcba6e929f
pohtayeeee <s6704053612244@email.kmutnb.ac.th>
2025-12-23 13:50:51 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById('display');
+
+function appendToDisplay(input) {
+    display.value += input;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        // ใช้ eval เพื่อคำนวณผลลัพธ์จาก string
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
